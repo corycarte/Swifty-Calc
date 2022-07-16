b4689cfc36bc5b6d7827368dc8b7b342050fa7b8
Cory Carte <7457451+corycarte@users.noreply.github.com>
2022-07-16 08:29:25 -0400

    Add XCode project to repo

---
+++ Swifty Calc/Swifty_CalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swifty_CalcApp.swift
+//  Swifty Calc
+//
+//  Created by Cory Carte on 7/16/22.
+//
+
+import SwiftUI
+
+@main
+struct Swifty_CalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
